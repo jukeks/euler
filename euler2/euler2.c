5656e6f647b8f4317d5093402290a042c20830c0
Jukka-Pekka Virtanen <virtanen@jukk.is>
2014-11-11 23:46:13 +0200

    Wrapped fibonacci history to a struct.

--- euler2/euler2.c
+++ euler2/euler2.c
@@ -4,29 +4,30 @@
 
 #define MAX_FIB_NUM (4U * 1000U * 1000U)
 
+typedef struct {
+	uint32_t current, previous;
+} fibonacci_history_t;
+
 /*
 	Calculates next value in Fibonacci series and updates parameters.
 	Behaviour when passing NULL to this function is undefined.
 */
-uint32_t next_fibonacci_number(uint32_t* current, uint32_t* previous) {
-	uint32_t next = *current + *previous;
+uint32_t next_fibonacci_number(fibonacci_history_t *history) {
+	uint32_t next = history->current + history->previous;
 
 	// updating history
-	*previous = *current;
-	*current = next;
+	history->previous = history->current;
+	history->current = next;
 
 	return next;
 }
 
 uint64_t sum_of_fibonacci_numbers_divisible_by_two_under(uint64_t under) {
 	// In the assignment, series starts with 1, 2
-	uint32_t fn = 1;
-	uint32_t f1 = 1;
-	uint32_t f2 = 1;
-
-	uint64_t sum = 0;
+	fibonacci_history_t history = {1, 1};
 	
-	for (; fn < under; fn = next_fibonacci_number(&f1, &f2)) {
+	uint64_t sum = 0;
+	for (uint32_t fn = 1; fn < under; fn = next_fibonacci_number(&history)) {
 		if (fn % 2 == 0) {
 			sum += fn;
 		}
